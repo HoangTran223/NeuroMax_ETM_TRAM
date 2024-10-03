12fb96e7a2d6f9e228f7ddd0548929154e15bc3c
HoangTran223 <tranvuonghoang204@gmail.com>
2024-10-03 13:18:27 +0700

    Update

--- basic_trainer.py
+++ basic_trainer.py
@@ -91,7 +91,8 @@ class BasicTrainer:
                 batch_loss.backward()
 
                 if (batch_idx + 1) % accumulation_steps == 0 or (batch_idx + 1) == len(dataset_handler.train_dataloader):
-                    theta, _ = self.model.get_theta(batch_data[0].to('cuda'))
+                    # theta, _ = self.model.get_theta(batch_data[0].to('cuda'))
+                    theta, _, a = self.model.get_theta(batch_data[0])
                     loss_ctr_ = self.model.get_loss_CTR(theta, indices)
                     sam_optimizer.first_step(loss_ctr_,
                                             zero_grad=True)
