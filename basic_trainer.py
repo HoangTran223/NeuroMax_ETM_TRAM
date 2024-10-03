00b52fb07692df15f5fff03aff0d3a70bd6d1c57
HoangTran223 <tranvuonghoang204@gmail.com>
2024-10-03 13:15:36 +0700

    Update

--- basic_trainer.py
+++ basic_trainer.py
@@ -44,7 +44,7 @@ class BasicTrainer:
         optimizer = TRAM(
             self.model.parameters(),
             base_optimizer, device=self.device,
-            lr=self.learning_rate, rho=self.rho,
+            lr=self.learning_rate,
             sigma=self.sigma, lmbda=self.lmbda
             )
         return optimizer
