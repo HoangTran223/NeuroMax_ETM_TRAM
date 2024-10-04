628fdb49c3fec5c5211847ce768a3477b2d6fa63
HoangTran223 <tranvuonghoang204@gmail.com>
2024-10-04 15:32:57 +0700

    Update

--- basic_trainer.py
+++ basic_trainer.py
@@ -94,9 +94,10 @@ class BasicTrainer:
             # if epoch > self.threshold: is_CTR = True      
             # else: is_CTR = False
 
-            for batch_idx, batch in enumerate(dataset_handler.train_dataloader): 
-                *inputs, indices = batch
-                batch_data = inputs
+            # for batch_idx, batch in enumerate(dataset_handler.train_dataloader): 
+            for batch_idx, batch_data in enumerate(dataset_handler.train_dataloader): 
+                *inputs, indices = batch_data
+                # batch_data = inputs
 
                 if self.model_name == 'NeuroMax':
                     rst_dict = self.model(indices, batch_data, epoch_id=epoch)
